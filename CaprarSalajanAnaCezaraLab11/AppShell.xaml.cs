93504c156c9850ab72cb46a1e9ded2b02ab8b88e
ANA-CEZARA CĂPRAR-SĂLĂJAN <ana.caprar@stud.ubbcluj.ro>
2024-12-19 18:45:28 +0200

    Add project files.

---
+++ CaprarSalajanAnaCezaraLab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CaprarSalajanAnaCezaraLab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CaprarSalajanAnaCezaraLab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaprarSalajanAnaCezaraLab11.Models;
+
+namespace CaprarSalajanAnaCezaraLab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
